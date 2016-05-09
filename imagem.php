677bdacdbd25ad87c6c7f0987ca9bc4125c5ecfe
Heitor <heitor@institutosoma.org.br>
2016-05-09 12:23:03 -0300

    Gerando link para usar no Gmail.

--- imagem.php
+++ imagem.php
@@ -3,14 +3,28 @@ $font       = 'assets/fonts/montserrat.ttf';
 $imagem     = imagecreatefromjpeg('assets/img/fundo_assinatura.jpg');
 $cor_nome   = imagecolorallocate($imagem, 35, 169, 56);
 $cor_funcao = imagecolorallocate($imagem, 75, 75, 75);
+$cor_info   = imagecolorallocate($imagem, 102, 102, 102);
 
-$nome   = mb_strtoupper(urldecode($_GET['nome']));
+$nome   = urldecode($_GET['nome']);
+$nome_u = mb_strtoupper($nome);
 $funcao = urldecode($_GET['funcao']);
 
 // imagestring($imagem, 5, 191, 24, $nome, $cor_nome);
 // imagestring($imagem, 3, 191, 50, $funcao, $cor_funcao);
-imagettftext($imagem, 13, 0, 191, 39, $cor_nome, $font, $nome);
+imagettftext($imagem, 13, 0, 191, 39, $cor_nome, $font, $nome_u);
 imagettftext($imagem, 10, 0, 191, 61, $cor_funcao, $font, $funcao);
+
+imagettftext($imagem, 8, 0, 204, 87, $cor_info, $font, 'Rua Gerson França, 14-77 - Vila Mesquita');
+imagettftext($imagem, 8, 0, 204, 101, $cor_info, $font, 'CEP 17014-380 - Bauru/SP');
+
+imagettftext($imagem, 8, 0, 204, 122, $cor_info, $font, '(14) 3227-4367 / 3206-8024 / 3879-8024');
+
+$filename     = 'assinatura_'.strtolower(str_replace('+', '_', urlencode($nome_u))).'.jpg';
+$filename_md5 = md5($nome.$funcao).'.jpg';
+$path         = $_SERVER['DOCUMENT_ROOT'].str_replace('imagem.php', '', $_SERVER['PHP_SELF']).'assets/upload/';
+if (!file_exists($path.$filename_md5)) {
+  imagejpeg($imagem, $path.$filename_md5, 100);
+}
 
 header('Content-type: image/jpeg');
 if (isset($_GET['download']) && $_GET['download']) {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let base_url = 'http://institutosoma.org.br/assinatura/';
+
 var SignatureForm = React.createClass({
   generateUrl: function(nome, funcao) {
     return ('imagem.php?nome='+nome+'&funcao='+funcao);
@@ -5,35 +7,50 @@ var SignatureForm = React.createClass({
   generateUrlDownload: function(nome, funcao) {
     return ('imagem.php?nome='+nome+'&funcao='+funcao+'&download=true');
   },
+  generateMd5: function(nome, funcao) {
+    return (base_url+'assets/upload/'+md5(nome+funcao)+'.jpg');
+  },
   getInitialState: function() {
     let nome   = 'Sandro dos Santos';
     let funcao = 'Gestor de SDS';
     let url    = this.generateUrl(nome, funcao);
+
     let url_download = this.generateUrlDownload(nome, funcao);
+    let url_amigavel = this.generateMd5(nome, funcao);
 
     return {
       nome:   nome,
       funcao: funcao,
       url:    url,
-      url_download: url_download
+      url_download: url_download,
+      url_amigavel: url_amigavel
     };
   },
   handleNomeChange: function(e) {
     let nome = e.target.value;
-    let url  = this.generateUrl(nome, this.state.funcao);
-    let url_download = this.generateUrlDownload(nome, this.state.funcao);
-
-    this.setState({nome: nome, url: url, url_download: url_download});
+    this.setState({nome: nome});
   },
   handleFuncaoChange: function(e) {
     let funcao = e.target.value;
-    let url    = this.generateUrl(this.state.nome, funcao);
-    let url_download = this.generateUrlDownload(this.state.nome, funcao);
-
-    this.setState({funcao: funcao, url: url, url_download: url_download});
+    this.setState({funcao: funcao});
   },
   handleSubmit: function(e) {
     e.preventDefault();
+
+    let nome   = this.state.nome;
+    let funcao = this.state.funcao;
+
+    let url          = this.generateUrl(nome, funcao);
+    let url_download = this.generateUrlDownload(nome, funcao);
+    let url_amigavel = this.generateMd5(nome, funcao);
+
+    this.setState({
+      url:          url,
+      url_download: url_download,
+      url_amigavel: url_amigavel
+    });
+
+    return false;
   },
   render: function() {
     return (
@@ -53,14 +70,19 @@ var SignatureForm = React.createClass({
                 <input type="text" placeholder="Sua função" className="form-control" id="inputFuncao" value={this.state.funcao} onChange={this.handleFuncaoChange} />
               </div>
             </div>
+            <div className="text-right">
+              <button type="submit" className="btn btn-primary btn-raised">Gerar Assinatura</button>
+            </div>
           </fieldset>
         </form>
         <br />
         <hr />
         <h3>Assinatura gerada</h3>
-        <a href={this.state.url_download} className="btn btn-primary btn-raised pull-right"><span className="glyphicon glyphicon-save"></span> Baixar imagem</a>
-        <img src={this.state.url} />
-        <span className="label label-default">Clique com o botão direito na imagem e então em "Salvar imagem como..." para salvar a imagem em seu computador.</span>
+        <a href={this.state.url_amigavel} target="_blank" id="urlAmigavel"><span className="glyphicon glyphicon-link"></span> {this.state.url_amigavel}</a>
+        <div>
+          <img src={this.state.url} />
+        </div>
+        <a href={this.state.url_download} className="btn btn-primary btn-raised hide"><span className="glyphicon glyphicon-save"></span> Baixar imagem</a>
       </div>
     );
   }
